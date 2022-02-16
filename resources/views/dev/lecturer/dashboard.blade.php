ff6e50cb142805ce1ad8302325e6583236206940
mhaikalazizan <mhaikalazizan@gmail.com>
2022-02-16 09:04:50 +0800

    added line

--- resources/views/dev/lecturer/dashboard.blade.php
+++ resources/views/dev/lecturer/dashboard.blade.php
@@ -16,6 +16,9 @@
                         <div class="col-md-12">
                             <a data-toggle="modal" data-target="#addLecturer" href="#" class="btn btn-primary"><i class="fa fa-plus"></i> Add New Lecturer</a>
                         </div>
+
+                        <hr />
+
                         <div class="col-md-12">
                             @if(Session::has('success'))
                             <div class="alert alert-success">{{Session::get('success')}}</div>
