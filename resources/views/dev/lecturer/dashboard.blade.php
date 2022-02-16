9341ab5aff12334bb7e8c0b41b0d7d98a465f623
mhaikalazizan <mhaikalazizan@gmail.com>
2022-02-16 09:10:19 +0800

    added margin

--- resources/views/dev/lecturer/dashboard.blade.php
+++ resources/views/dev/lecturer/dashboard.blade.php
@@ -17,9 +17,7 @@
                             <a data-toggle="modal" data-target="#addLecturer" href="#" class="btn btn-primary"><i class="fa fa-plus"></i> Add New Lecturer</a>
                         </div>
 
-                        <hr />
-
-                        <div class="col-md-12">
+                        <div class="col-md-12" style="margin-top: 10px;">
                             @if(Session::has('success'))
                             <div class="alert alert-success">{{Session::get('success')}}</div>
                             @elseif(Session::has('error'))
