6141ff86874bcdb353e3f01de06d859c2ef1ad4c
mhaikalazizan <mhaikalazizan@gmail.com>
2022-02-27 07:13:08 +0800

    added new view

---
+++ resources/views/lecturer/student_list.blade.php
@@ -0,0 +1,47 @@
+@extends('lecturer.layout.app')
+
+@section('title', 'Student List')
+
+@section('content')
+
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">@yield('title')</h1>
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <!-- Modal -->
+                            <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Confirmation</h5>
+                                        </div>
+                                        <div class="modal-body">
+                                            Are you sure want to log out?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-danger" onclick="window.location='/lecturer/logout';">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+
+        
+
+
+@stop
+
+            
