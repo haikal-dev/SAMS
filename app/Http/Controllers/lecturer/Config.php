b88242e16861f57ee806b5e483296a63913df366
haikal-dev <mhaikalazizan@gmail.com>
2022-02-26 08:40:48 +0000

    add config controller

---
+++ app/Http/Controllers/lecturer/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\lecturer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Config extends Controller
+{
+    public $sessionName = "SAMS_LECTURER";
+    public $homeUrl = "dev";
+
+    public function __construct(){
+        $this->homeUrl = env('APP_URL') . "/" . $this->homeUrl;
+    }
+}
