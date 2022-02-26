6a3a8ecef9bdb6e77f4d511e23026418ef01083a
mhaikalazizan <mhaikalazizan@gmail.com>
2022-02-27 07:01:34 +0800

    add student id

--- database/migrations/2022_02_26_222507_create_students_table.php
+++ database/migrations/2022_02_26_222507_create_students_table.php
@@ -16,6 +16,7 @@ class CreateStudentsTable extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->text('fullname');
+            $table->text('student_id');
             $table->text('phone');
             $table->text('email');
             $table->text('password');
