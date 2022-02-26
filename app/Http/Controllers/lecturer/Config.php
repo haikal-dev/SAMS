74cbfbf6bfaaad95f1feb7662298c47df9fdf09d
haikal-dev <mhaikalazizan@gmail.com>
2022-02-26 09:11:00 +0000

    change homeUrl

--- app/Http/Controllers/lecturer/Config.php
+++ app/Http/Controllers/lecturer/Config.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class Config extends Controller
 {
     public $sessionName = "SAMS_LECTURER";
-    public $homeUrl = "dev";
+    public $homeUrl = "lecturer";
 
     public function __construct(){
         $this->homeUrl = env('APP_URL') . "/" . $this->homeUrl;
