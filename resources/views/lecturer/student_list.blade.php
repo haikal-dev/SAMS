e92e4635e3f1bb43a917aa84f70fe3286a12a992
mhaikalazizan <mhaikalazizan@gmail.com>
2022-03-05 19:57:41 +0800

    add new button and table

--- resources/views/lecturer/student_list.blade.php
+++ resources/views/lecturer/student_list.blade.php
@@ -10,6 +10,23 @@
                             <h1 class="page-header">@yield('title')</h1>
                         </div>
                         <!-- /.col-lg-12 -->
+                        <div class="col-lg-12">
+                            <a href="{{$config->homeUrl}}/lecturer/student/add"><i class="fa fa-plus"></i> Add New Student</a>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>E-mail</th>
+                                        <th>Phone</th>
+                                        <th>Class</th>
+                                        <th>#</th>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
                     </div>
 @stop
 
