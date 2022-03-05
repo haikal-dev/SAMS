c8e99c03878c36a086ef01dcf5248ebef6b335b5
mhaikalazizan <mhaikalazizan@gmail.com>
2022-03-05 20:04:46 +0800

    improve table layout and fix typo link

--- resources/views/lecturer/student_list.blade.php
+++ resources/views/lecturer/student_list.blade.php
@@ -11,19 +11,22 @@
                         </div>
                         <!-- /.col-lg-12 -->
                         <div class="col-lg-12">
-                            <a href="{{$config->homeUrl}}/lecturer/student/add" class="btn btn-primary"><i class="fa fa-plus"></i> Add New Student</a>
+                            <a href="{{$config->homeUrl}}/student/add" class="btn btn-primary"><i class="fa fa-plus"></i> Add New Student</a>
                         </div>
                         <div class="col-lg-12" style="margin-top: 10px;">
                             <div class="table-responsive">
                                 <table class="table table-striped">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Name</th>
-                                        <th>E-mail</th>
-                                        <th>Phone</th>
-                                        <th>Class</th>
-                                        <th>#</th>
-                                    </tr>
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Name</th>
+                                            <th>E-mail</th>
+                                            <th>Phone</th>
+                                            <th>Class</th>
+                                            <th>#</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
                                 </table>
                             </div>
                         </div>
